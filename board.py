c1d2119722b91abc7932b01e839e2dc980cad470
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2025-01-18 16:22:10 +1100

    added function to add token to board

--- board.py
+++ board.py
@@ -2,12 +2,16 @@ class Board:
     def __init__(self, rows, cols):
         self.board = [[0 for _ in range(cols)] for _ in range(rows)]
 
-    def print(self):
+    def __str__(self):
+        board_str = ""
         for row in range(len(self.board)):
             for col in range(len(self.board[row])):
-                print(self.board[row][col], end=' ')
-            print()
+                board_str += f"{self.board[row][col]} "
+            board_str += "\n"
+        return board_str
 
-
-b = Board(6, 7)
-b.print()
+    def place_token(self, col, token):
+        for i in range(len(self.board) - 1, 0, -1):
+            if self.board[i][col] == 0:
+                self.board[i][col] = token
+                return
